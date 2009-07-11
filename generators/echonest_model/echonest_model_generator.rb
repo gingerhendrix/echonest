a52761b65b1aadc437802a4783ef47ee30ab7ee7
Gareth Andrew <gingerhendrix@hotmail.com>
2009-07-11 16:26:43 +0100

    Added echonest model generator

---
+++ generators/echonest_model/echonest_model_generator.rb
@@ -0,0 +1,52 @@
+class EchonestModelGenerator < RubiGen::Base
+
+  default_options :author => nil
+
+  attr_reader :name
+
+  def initialize(runtime_args, runtime_options = {})
+    super
+    usage if args.empty?
+    @name = args.shift
+    extract_options
+  end
+
+  def manifest
+    record do |m|
+      # Ensure appropriate folder(s) exists
+      m.directory 'lib/echonest'
+
+      m.template "result.rb.erb", "lib/echonest/#{name}_result.rb"
+      m.template "results.rb.erb", "lib/echonest/#{name}_results.rb"
+      m.template "doc.rb.erb", "lib/echonest/#{name}_doc.rb"
+      
+    end
+  end
+
+  protected
+    def banner
+      <<-EOS
+Creates a ...
+
+USAGE: #{$0} #{spec.name} name
+EOS
+    end
+
+    def add_options!(opts)
+      # opts.separator ''
+      # opts.separator 'Options:'
+      # For each option below, place the default
+      # at the top of the file next to "default_options"
+      # opts.on("-a", "--author=\"Your Name\"", String,
+      #         "Some comment about this option",
+      #         "Default: none") { |o| options[:author] = o }
+      # opts.on("-v", "--version", "Show the #{File.basename($0)} version number and quit.")
+    end
+
+    def extract_options
+      # for each option, extract it into a local variable (and create an "attr_reader :author" at the top)
+      # Templates can access these value via the attr_reader-generated methods, but not the
+      # raw instance variable value.
+      # @author = options[:author]
+    end
+end
